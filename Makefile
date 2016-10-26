c7bd99ecb0b3fd16a41571725f8349ca11d3c736
Sean Batzel <stbatzel@gmail.com>
2016-10-26 10:57:03 -0400

    Started building a client. Sockets are not going to be easy.

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
 make:
 	mkdir build
 	mkdir build/server
+	mkdir build/client
+	mkdir build/client/linux
 	cc src/server/server.c -g -Wall -Wextra -o build/server/server
+	cc src/client/linux/client.c -g -Wall -Wextra -o build/client/linux/client
 
 clean:
 	rm -rf build
-
-linux:
-
-
-server:
--- src/client/linux/client.c
+++ src/client/linux/client.c
@@ -1,3 +1,10 @@
+/*
+ * Client utilizing the GIPS protocol designed by Sean Batzel and Andrew Plaza.
+ *
+ * Client designed by Sean Batzel
+ */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -11,168 +18,62 @@
 #include <netdb.h>
 #include <pthread.h>
 
-//#define HOST "server2.cs.scranton.edu"
+#define HOST "server1.cs.scranton.edu"
 #define HTTPPORT "32200"
 #define BACKLOG 10
 
-int get_server_socket(char *hostname, char *port) {
-	struct addrinfo hints, *servinfo, *p;
-	int status;
-	int server_socket;
-	int yes = 1;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = PF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-
-	if ((status = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
-		printf("getaddrinfo: %s\n", gai_strerror(status));
-		exit(1);
-	}
-
-	for (p = servinfo; p != NULL; p = p ->ai_next) {
-		if ((server_socket = socket(p->ai_family, p->ai_socktype,
-						p->ai_protocol)) == -1) {
-			printf("socket socket \n");
-			continue;
-		}
-		if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-			printf("socket option\n");
-			continue;
-		}
-
-		if (bind(server_socket, p->ai_addr, p->ai_addrlen) == -1) {
-			printf("socket bind \n");
-			continue;
-		}
-		break;
-	}
-	print_ip(servinfo);
-	freeaddrinfo(servinfo);
-	return server_socket;
+int translate_to_gips(int x, int y) {
+	
 }
 
-int start_server(int serv_socket, int backlog) {
-	int status = 0;
-	if ((status = listen(serv_socket, backlog)) == -1) {
-		printf("socket listen error\n");
-	}
-	return status;
+int translate_from_gips(int gips) {
+
 }
 
-int accept_client(int serv_sock) {
-	int reply_sock_fd = -1;
-	socklen_t sin_size = sizeof(struct sockaddr_storage);
-	struct sockaddr_storage client_addr;
-	char client_printable_addr[INET6_ADDRSTRLEN];
+int send_gips_to_server(int gips) {
 
-	if ((reply_sock_fd = accept(serv_sock, 
-					(struct sockaddr *)&client_addr, &sin_size)) == -1) {
-		printf("socket accept error\n");
-	}
-	else {
-		inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *)&client_addr), 
-				client_printable_addr, sizeof client_printable_addr);
-		printf("server: connection from %s at port %d\n", client_printable_addr,
-				((struct sockaddr_in*)&client_addr)->sin_port);
-	}
-	return reply_sock_fd;
 }
 
-void start_subserver(int reply_sock_fd) {
-	pthread_t pthread;
-	long reply_sock_fd_long = reply_sock_fd;
-	if (pthread_create(&pthread, NULL, subserver, (void *)reply_sock_fd_long) != 0) {
-		printf("failed to start subserver\n");
-	}
-	else {
-		printf("subserver %ld started\n", (unsigned long)pthread);
-	}
+int receive_gips_from_server() {
+
 }
 
-void *subserver(void * reply_sock_fd_as_ptr) {
-	char *html_file;
-	int html_file_fd;
-	int read_count = -1;
-	int BUFFERSIZE = 256;
-	char buffer[BUFFERSIZE+1];
+void display_board(int board1, board2) {
 
-	long reply_sock_fd_long = (long) reply_sock_fd_as_ptr;
-	int reply_sock_fd = (int) reply_sock_fd_long;
-	printf("subserver ID = %ld\n", (unsigned long) pthread_self());
-	read_count = recv(reply_sock_fd, buffer, BUFFERSIZE, 0);
-	buffer[read_count] = '\0';
-	printf("%s\n", buffer);
-	html_file = "../pages/";
-	html_file = strtok(&buffer[5], " \t\n");
-	printf("FILENAME: %s\n", html_file);
-	html_file_fd = open(html_file, O_RDONLY);
-	while ((read_count = read(html_file_fd, buffer, BUFFERSIZE))>0) {
-		send(reply_sock_fd, buffer, read_count, 0);
-	}
-	close(reply_sock_fd);
-
-	return NULL;
 }
 
-void print_ip( struct addrinfo *ai) {
-	struct addrinfo *p;
-	void *addr;
-	char *ipver;
-	char ipstr[INET6_ADDRSTRLEN];
-	struct sockaddr_in *ipv4;
-	struct sockaddr_in6 *ipv6;
-	short port = 0;
-
-	for (p = ai; p !=  NULL; p = p->ai_next) {
-		if (p->ai_family == AF_INET) {
-			ipv4 = (struct sockaddr_in *)p->ai_addr;
-			addr = &(ipv4->sin_addr);
-			port = ipv4->sin_port;
-			ipver = "IPV4";
-		}
-		else {
-			ipv6= (struct sockaddr_in6 *)p->ai_addr;
-			addr = &(ipv6->sin6_addr);
-			port = ipv4->sin_port;
-			ipver = "IPV6";
-		}
-		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-		printf("serv ip info: %s - %s @%d\n", ipstr, ipver, ntohs(port));
+int connect_to_server() {
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	struct hostent serv = gethostbyname(HOST);
+	int succ = connect(sock, serv, sizeof(serv));
+	if (succ != -1) {
+		return sock;
 	}
+	return -1;
 }
 
-void *get_in_addr(struct sockaddr * sa) {
-	if (sa->sa_family == AF_INET) {
-		printf("ipv4\n");
-		return &(((struct sockaddr_in *)sa)->sin_addr);
+int main() {
+	int board1 = 0;
+	int board2 = 0;
+	char *name;
+	int move_x;
+	int move_y;
+	int gips;
+	int sock = connect_to_server();
+	printf("Gomoku Client for Linux\n");
+	if (sock != -1) {
+		printf("Enter your name: ");
+		scanf("%s", name);
 	}
-	else {
-		printf("ipv6\n");
-		return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+	while (board != 999) {
+		display_board();
+		printf("%s> ", name);
+		scanf("%d %d", &move_x, &move_y);
+		gips = translate_to_gips(move_x, move_y);
+		send_gips_to_server(gips);
+		board1 = receive_gips_from_server();
+		board2 = receive_gips_from_server();
 	}
+	close(sock);
+	return 0;
 }
-
-int main(void)
-{
-	int http_sock_fd;
-	int reply_sock_fd;
-	int yes;
-
-	http_sock_fd = get_server_socket(HOST, HTTPPORT);
-
-	if (start_server(http_sock_fd, BACKLOG) == -1) {
-		printf("start server error\n");
-		exit(1);
-	}
-
-	while(1) {
-		if ((reply_sock_fd = accept_client(http_sock_fd)) == -1) {
-			continue;
-		}
-
-		start_subserver(reply_sock_fd);
-	}
-} 
-
