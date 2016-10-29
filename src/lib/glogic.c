c05d87b7a47702e3edbc752dab5c9c080daacf46
Sean Batzel <stbatzel@gmail.com>
2016-10-29 02:45:28 -0400

    This is drastically overcomplicated.

--- src/lib/glogic.c
+++ src/lib/glogic.c
@@ -4,10 +4,11 @@
 void check_for_win_server(gips *x) {
 	// Return 1 if yes, 0 if no.
 	char *board = unpack(x);
-	x->is_win = 0;
+	x->is_win = crawl_board(board);
 }
 
 int check_for_win_client(gips *x) {
 	// We may not need this after all...
 	return 0;
 }
+
