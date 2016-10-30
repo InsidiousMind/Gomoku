ae5d6c8c4414ff26fce0dc183728ad21be1afb25
Sean Batzel <stbatzel@gmail.com>
2016-10-29 21:35:48 -0400

    I really don't think this is right.

--- src/lib/glogic.c
+++ src/lib/glogic.c
@@ -3,23 +3,32 @@
 
 void check_for_win_server(gips *x) {
 	// Set x->is_win to 0 if nobody won, otherwise set it to the
-	// Player number of the winner..
+	// Player number of the winner.
 	char *board = unpack(x);
 	x->is_win = crawl_board(board);
 }
 
-short crawl_board(char *board) {
-	struct node {
-		short player;
-		struct node child0;
-		struct node child1;
-		struct node child2;
-		struct node child3;
-		struct node child4;
-		struct node child5;
-		struct node child6;
-		struct node child7;
-	};
-	
-	typedef struct node Node;
+char crawl_board(char *board) {
+	/*
+	 * In C, chars are just ascii values stored in a byte of memory.
+	 * We don't need a full integer, so we're going to leverage the
+	 * manner in which chars are represented to provide us with a 
+	 * smaller memory footprint.
+	 */
+	char i = 0;
+	char j = 0;
+	char k = 0;
+	char l = 0;
+	while (i < 8) {
+		while (j < 8) {
+			while ((i + k >= 0) && (i + k < 8)) {
+				while ((j + l >= 0) && (j + l < 8)) {
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
 }
--- src/lib/glogic.h
+++ src/lib/glogic.h
@@ -1,4 +1,5 @@
 #ifndef GLOGIC
 #define GLOGIC
 void check_for_win_server(gips *x);
+char crawl_board(char *board);
 #endif
