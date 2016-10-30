64b607f603753081f6cd5031591ce8d35a32574c
Andrew Plaza <andrew.plaza15@gmail.com>
2016-10-30 11:16:58 -0400

    idea for game logic, added some possibilities that may be of use in GIPS. still figuring that gips thing out.

--- src/lib/glogic.c
+++ src/lib/glogic.c
@@ -9,6 +9,35 @@ void check_for_win_server(gips *x, char **board) {
 
 }
 
+
+
+//two fors w/ two whiles might be possible to avoid
+//
+/*
+ * Maybe something like this? instead of iterating through the entire board, we can just check if the latest move results in five-in-a-row.
+ * this sets up two arrays with the directoins we need, and loops in that direction until it hits five-in-a-row or the edge of the board
+ * needs some modification but basically this code:
+ *
+ *  static const int xdirs[] = {0,1,1,1,0,-1,-1,-1}; //these are the eight directions that we need to check (up down right left diagnals)
+ *  static const int ydirs[] = {-1,-1,0,1,1,1,0,-1};
+ *  for (int i=0; i<8; i++)  {
+ *     int x = startx, y = starty, numInARow=0;
+ *     for (int j = 0; j < 5 && IsWithinBoard(x, y); j++)
+ *     {
+ *       // Test this cell here, maybe increment numInARow 
+ * 
+ *      x += xdirs[i];
+ *      y += ydirs[i];
+ *     }
+ *  }
+ *
+ *
+ *
+ *
+ *
+ */
+//also we can always use ints and just return a casted (char)int
+//might be more proper
 char crawl_board(char **board) {
 	/*
 	 * In C, chars are just ascii values stored in a byte of memory.
