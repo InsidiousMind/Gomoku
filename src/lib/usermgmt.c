83dec756fed15691a5f11e70e9624d2831ead8d5
Sean Batzel <stbatzel@gmail.com>
2016-11-09 15:42:40 -0500

    I feel like I'm moving backward.

--- src/lib/usermgmt.c
+++ src/lib/usermgmt.c
@@ -1,7 +1,9 @@
-int login() {
-
+int login(int sock, char *username) {
+  int pid;
+  //get pid from the server based on the username.
+  return pid;
 }
 
-int server_add_user() {
+int server_add_user(int sock, char *username) {
 
 }
