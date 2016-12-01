c2c83810ef013ae187cf2df4e090ff0dc9b3b903
Andrew Plaza <andrew.plaza@scranton.edu>
2016-12-01 16:53:52 -0500

    fixed some compilation errors on freeBSD, namely fixing the includes

--- src/server/commons/asgn6-server.h
+++ src/server/commons/asgn6-server.h
@@ -3,11 +3,13 @@
 #ifndef BACKLOG
 #define BACKLOG 10
 #endif
+
 #ifndef NUM_THREADS
 #define NUM_THREADS 2
 #endif
-#ifndef ASGN6-SERVER_H
-#define ASGN6-SERVER_H
+
+#ifndef ASGN6SERVER_H
+#define ASGN6_H
 void serverLoop(int fd, Node **head, pthread_mutex_t *head_access);
 int accept_client(int serv_sock);
-#define ASGN6-SERVER_H
+#endif //ASGN6SERVER_H
--- src/server/main.c
+++ src/server/main.c
@@ -23,6 +23,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
 #include "../lib/database.h"
